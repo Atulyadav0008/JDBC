fff8d19cc62addd469c41ccbc65adc15a7750f40
Atulyadav0008 <singhofsaidpur@gmail.com>
2026-02-12 11:58:12 +0530

    Add reservation function added

--- src/com/JDBC/DataUpdation.java
+++ src/com/JDBC/DataUpdation.java
@@ -10,9 +10,8 @@ public class DataUpdation {
     	String url = "jdbc:mysql://127.0.0.1:3306/mydatabase";
     	String username = "root";
     	String password = "15062005Ay@";
-//    	String query = "UPDATE employees\n"+
-//    	"SET job_title = 'Full stack developer', salary = 70000.0\n" + "Where id = 2;";
-    	String query = "select * from employees;";
+    	String query = "UPDATE employees\n"+
+    	"SET job_title = 'Full stack developer', salary = 70000.0\n" + "Where id = 2;";
     	
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -26,28 +25,15 @@ public class DataUpdation {
         	Connection con = DriverManager.getConnection(url,username,password);
         	System.out.println("Connection established successfully");
         	Statement stmt = con.createStatement();
-//        	int rowsAffected = stmt.executeUpdate(query);
-        	ResultSet rs = stmt.executeQuery(query);
-//        	if(rowsAffected > 0) {
-//        		System.out.println("Updation succssful" + rowsAffected + " rows(s).");
-//        	}else {
-//        		System.out.println("Updation failed");
-//        	}
+        	int rowsAffected = stmt.executeUpdate(query);
+        	
+        	if(rowsAffected > 0) {
+        		System.out.println("Updation succssful" + rowsAffected + " rows(s).");
+        	}else {
+        		System.out.println("Updation failed");
+        	}
         			
-        			while(rs.next()) {
-                		int id = rs.getInt("id");
-                		String name = rs.getString("name");
-                		String job_title = rs.getString("job_title");
-                		double salary = rs.getDouble("salary");
-                		
-                		System.out.println("----------------");
-                		System.out.println("id : " + id);
-                		System.out.println("name : " + name);
-                		System.out.println("job_title : " + job_title);
-                		System.out.println("salary : " + salary);
-                		
-                		
-                	}
+        			
         
         	
         	
--- src/com/JDBC/HotelreservationSystem.java
+++ src/com/JDBC/HotelreservationSystem.java
@@ -1,5 +1,82 @@
 package com.JDBC;
 
-public class HotelreservationSystem {
+import java.sql.Connection;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.Scanner;
+
+public class HotelreservationSystem {
+	 public static void main(String[] args) throws ClassNotFoundException , SQLException {
+	    	
+	        private static final String url = "jdbc:mysql://127.0.0.1:3306/mydatabase";
+	        private static final String username = "root";
+	        private static final String password = "15062005Ay@";
+	    
+	    	
+	    	
+	        try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	          
+	        } catch (ClassNotFoundException e) {
+	            System.out.println(e.getMessage());
+	        }
+	        
+	        
+	        try {
+	        	Connection con = DriverManager.getConnection(url,username,password);
+               while(true) {
+            	   System.out.println();
+            	   System.out.println("Hotel Reservation System : ");
+            	   Scanner sc = new Scanner(System.in);
+            	   System.out.println("1. Reserve a room. ");
+            	   System.out.println("2. Check Reservation. ");
+            	   System.out.println("3. Get Room Number. ");
+            	   System.out.println("4. Update reservation. ");
+            	   System.out.println("5. Delete Reservation. ");
+            	   System.out.println("6. Exit. ");
+            	   System.out.println("Choose an option--->>>.");
+            	   	int choice = sc.nextInt();
+            	   	
+            	   	switch(choice) {
+            	   	case 1:
+            	   		reserveRoom(con,sc);
+            	   		break;
+            	   	case 2:
+            	   		checkreservation(con,sc);
+            	   		break;
+            	   	case 3:
+            	   	    getRoomNumber(con,sc);
+            	   	    break;
+            	   	case 4:
+            	   		updateReservation(con,sc);
+            	   		break;
+            	   	case 5:
+            	   		delatereservation(con,sc);
+            	   		break;
+            	   	case 6:
+            	   		exit();
+            	   		sc.close();
+            	   		return;
+            	   	default:
+            	   		
+            	   		
+            	   	}
+            	   	}
+            	   }
+	        	
+	        		
+	        	
+	        }catch(SQLException e) {
+	        	System.out.println(e.getMessage());
+	        }
+	    }
+
+	 private static void Switch() {
+		// TODO Auto-generated method stub
+		
+	 }
 }
